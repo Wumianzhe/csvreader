94380bf4eabc1b682786c639717d6fba0fc119b0
Alexandr Kopnov <a.kopnov24@gmail.com>
2023-02-10 07:56:06 +0300

    format tracked, main reformat

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
-#include "inout.h"
+#include "table.h"
 #include <iostream>
 
 using namespace std;
-int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    cout << "missing argument" << endl;
-    return 1;
-  }
-  string filename = argv[1];
-  try {
-    Table t = read(filename);
-    cout << t;
-  } catch (const std::exception &ex) {
-    cout << ex.what() << endl;
-  }
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "missing argument" << endl;
+        return 1;
+    }
+    string filename = argv[1];
+    try {
+        Table t = read(filename);
+        cout << t;
+    } catch (const std::exception& ex) {
+        cout << ex.what() << endl;
+    }
 
-  return 0;
+    return 0;
 }
